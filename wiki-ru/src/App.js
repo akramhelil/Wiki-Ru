dcab8f5c8e1e1dba043f04d68dab39011b833915
akramhelil <akramhelil@gmail.com>
2019-07-18 11:15:41 -0400

    react router complete & it renders the right information

--- wiki-ru/src/App.js
+++ wiki-ru/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import NavBar from './componesnts/NavBar';
-
+import { Route, Switch } from 'react-router-dom';
+import About from './componesnts/About';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <Switch>
+        <Route path='/about' component={ About } />
+      </Switch>
     </div>
   );
 }
